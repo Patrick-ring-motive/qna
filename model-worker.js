112596fe14d933737baa9271e7a9701cba3a7e8d
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-03-13 21:34:52 -0500

    Update model-worker.js

--- model-worker.js
+++ model-worker.js
@@ -1,4 +1,26 @@
 
+const lcs = function lcs(seq1, seq2) {
+    "use strict";
+    let arr1 = [...seq1 ?? []];
+    let arr2 = [...seq2 ?? []];
+    if (arr2.length > arr1.length) {
+        [arr1, arr2] = [arr2, arr1];
+    }
+    const dp = Array(arr1.length + 1).fill(0).map(() => Array(arr2.length + 1).fill(0));
+    const dp_length = dp.length;
+    for (let i = 1; i !== dp_length; i++) {
+        const dpi_length = dp[i].length;
+        for (let x = 1; x !== dpi_length; x++) {
+            if (arr1[i - 1] === arr2[x - 1]) {
+                dp[i][x] = dp[i - 1][x - 1] + 1
+            } else {
+                dp[i][x] = Math.max(dp[i][x - 1], dp[i - 1][x])
+            }
+        }
+    }
+    return dp[arr1.length][arr2.length]
+};
+
 
 self.onmessage = async (e) => {
                 const { type, payload } = e.data;
@@ -22,7 +44,25 @@ self.onmessage = async (e) => {
                 if (type === 'ASK') {
                     try {
                         const { question, context } = payload;
-                        const answers = await self.model.findAnswers(question, context);
+                        let answers = await self.model.findAnswers(question, context);
+                        const qarr = question.split(/\s+/);
+                        const carr = context.split(/\s+/);
+                        if(!answer?.length){
+                          for(let i = 0; i < qarr.length;i++){
+                            let word = qarr[0];
+                            let bestMatch = carr[0];
+                            let matchScore = lcs(word,bestMatch) * Math.min(word.length,carr[0].length) / Math.max(word.length,carr[0].length);
+                            for(const ctxword of carr){
+                              const score = lcs(word,ctxword) * Math.min(word.length,ctxword.length) / Math.max(word.length,ctxword.length);
+                              if(score > matchScore){
+                                matchScore = score;
+                                bestMatch = ctxword;
+                              }
+                            }
+                            qarr[i] = bestMatch;
+                          }
+                          answers = await self.model.findAnswers(qarr.join(' '), context);
+                        }
                         
                         // Apply the specific scoring logic requested
                         let bestAnswer = (answers && answers.length > 0) ? answers[0].text : "No answer found.";
